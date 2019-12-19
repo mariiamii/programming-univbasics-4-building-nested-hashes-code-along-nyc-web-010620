919fd3df8d6ac0994695175241ff2ab086121ef0
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-19 19:46:54 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -16,7 +16,7 @@ def third_challenge
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        
         },
       :matriarch => {
         #your key/value pairs here
