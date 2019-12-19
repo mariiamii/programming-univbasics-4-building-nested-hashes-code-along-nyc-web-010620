e6f193fecef43a2d4ad6d2725358162d628ee974
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-12-19 19:49:51 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,15 +1,9 @@
 def third_challenge
   epic_tragedy = {
    :montague => {
-      :patriarch => {
-        name: "Lord Montague", age: "53"
-        },
-      :matriarch => {
-        name: "Lady Montague", age: "54"
-        },
-      :hero => {
-        name: "Romeo", age: "15", status: "Alive"
-        },
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "Alive"},
       :hero_friends => []
    }, 
    :capulet => {
